1945d2ecf64f64d2d79b2c6944cd0c747df779f5
timothee-durand <timothee.durand70@gmail.com>
2022-02-12 18:41:21 +0100

    add font and component

---
+++ components/project-presentation.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="project-presentation" :data-techno="getJsonData()">
+    <slot></slot>
+  </div>
+</template>
+<script>
+
+export default {
+  name: "ProjectPresentation",
+  props: {
+    metadatas: {
+      type: Object,
+      default() {
+        return {
+          technos:
+            [
+              "VueJs",
+              "ThreeJS"
+            ]
+        };
+      }
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+    getJsonData() {
+      return JSON.stringify(this.metadatas.technos);
+    }
+  }
+
+};
+</script>
+<style lang="scss">
+.project-presentation {
+  width: 50rem;
+  height: 30rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+}
+
+.project-presentation > *:not(canvas) {
+  position: relative;
+  z-index: 1;
+}
+
+canvas {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
